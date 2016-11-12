393c71d9d8b1e7cd9c784bee313a7060e5d2041b
Jessica Joseph <Jessicajoseph@nyu.edu>
2016-11-12 04:09:12 -0500

    AR is working. time to add the markers & map

---
+++ yhealth/AR-BridgeHeader.h
@@ -0,0 +1,9 @@
+#import "ARKit.h"
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import "ARGeoCoordinate.h"
+#import "ARLocationDelegate.h"
+#import "ARViewProtocol.h"
+#import "GEOLocations.h"
+#import "AugmentedRealityController.h"
+
